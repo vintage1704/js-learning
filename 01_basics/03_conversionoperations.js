5441375598d7e2dda0fcf2230f8b6d39099b1079
vintage1704 <153293526+vintage1704@users.noreply.github.com>
2024-03-03 17:29:16 +0000

    conversion operators

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,12 +1,12 @@
 let score = null
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let ValueInNumber = Number(score)
 
-console.log(typeof score);
-console.log(typeof ValueInNumber);
-console.log(ValueInNumber);
+//console.log(typeof score);
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
  
 // "33" => 33
 // "33abc" => NaN
@@ -21,4 +21,45 @@ console.log(ValueInNumber);
 
 // 1 = true and 0=false
 // "" = false, "rakshit" = true
+
+// OPERATIONS
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// ** power hota hai
+// % remainder hota hai
+
+let str1 = "hello"
+let str2 = " Rakshit"
+
+//let str3 = str1 + str2
+
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + 2 + "2");
+
+// ***just remember that agar string first hai toh sab string mein convert hojaega 
+// agr string last mein hai toh pehle number add hoga then string ke sath hoga***
+
+//let num1 = num2 = num3
+
+//num1 = num2 = num3 = 2 + 2
+
+// let gamecounter = 100;
+
+// console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
 
