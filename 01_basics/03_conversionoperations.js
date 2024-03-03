2dbeea6e92a333472584fa69fc38fc7c1668cc42
vintage1704 <153293526+vintage1704@users.noreply.github.com>
2024-03-03 13:08:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,24 @@
+let score = null
+
+console.log(typeof score);
+
+let ValueInNumber = Number(score)
+
+console.log(typeof score);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+ 
+// "33" => 33
+// "33abc" => NaN
+
+// true = 1, false=0
+
+//let isloggedIn = 1
+ //let booleanform = Boolean(isloggedIn)
+
+//console.log(typeof booleanform);
+//console.log(booleanform);
+
+// 1 = true and 0=false
+// "" = false, "rakshit" = true
+
