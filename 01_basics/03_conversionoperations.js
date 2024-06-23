25686dfa3c4f769b4038029409881a308a3c6d42
vintage1704 <153293526+vintage1704@users.noreply.github.com>
2024-06-23 12:50:17 +0000

    conversion of datatypes

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,65 +1,11 @@
-let score = null
+let number = 33
 
-//console.log(typeof score);
+console.log(number)
 
-let ValueInNumber = Number(score)
+let score = "64"
 
-//console.log(typeof score);
-//console.log(typeof ValueInNumber);
-//console.log(ValueInNumber);
- 
-// "33" => 33
-// "33abc" => NaN
+console.log(typeof score)
 
-// true = 1, false=0
+let valueInNumber = Number(score)
 
-//let isloggedIn = 1
- //let booleanform = Boolean(isloggedIn)
-
-//console.log(typeof booleanform);
-//console.log(booleanform);
-
-// 1 = true and 0=false
-// "" = false, "rakshit" = true
-
-// OPERATIONS
-
-let value = 3
-let negValue = -value
-//console.log(negValue);
-
-// ** power hota hai
-// % remainder hota hai
-
-let str1 = "hello"
-let str2 = " Rakshit"
-
-//let str3 = str1 + str2
-
-//console.log(str3);
-
-//console.log("1" + 2);
-//console.log(1 + 2 + "2");
-
-// ***just remember that agar string first hai toh sab string mein convert hojaega 
-// agr string last mein hai toh pehle number add hoga then string ke sath hoga***
-
-//let num1 = num2 = num3
-
-//num1 = num2 = num3 = 2 + 2
-
-// let gamecounter = 100;
-
-// console.log(gamecounter);
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(typeof valueInNumber)
