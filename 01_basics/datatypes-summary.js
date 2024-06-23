def1924298d2ee32bff3782b8e8bf64797942199
vintage1704 <153293526+vintage1704@users.noreply.github.com>
2024-06-23 13:51:37 +0000

    some minor changes about stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,33 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+   // console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack memory(Primitive), Heap memeory (Non-primitive) --> These are the two types of memory
+
+// Heap mein original value mein change hoga whereas stack mein bas ek copy of the original data mei value change hoga
+
+
+let myName = "Rakshit"
+
+
+anotherName = myName
+
+
+
+anotherName = "Sonu"
+
+// console.log(anotherName)
+
+
+
+
